5e91ee228cdcfd408d3dbab517f4500d7cd5d263
Jeff Lee <leejefon@gmail.com>
2023-08-11 01:47:11 -0700

    Updated libraries to latest

--- src/app.jsx
+++ src/app.jsx
@@ -6,10 +6,8 @@
  */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './pages/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+const root = createRoot(document.getElementById('app'));
+root.render(<App />);
